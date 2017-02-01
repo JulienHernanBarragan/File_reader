c87abc9841dd435b9e219ad2521d7ba0e4674697
Julien <Julien@DESKTOP-OTEGEKM>
2017-02-01 20:24:20 +0100

    Change name for Main

--- FileReader/src/lectureFichier/Main.java
+++ FileReader/src/lectureFichier/Main.java
@@ -2,7 +2,7 @@ package lectureFichier;
 
 import java.io.*;
 
-public class ReadFile {
+public class Main {
 	public static void main(String[] args){
 		
 		Read_file session = new Read_file().read_file("C:/Users/Julien/Desktop/test.txt");
